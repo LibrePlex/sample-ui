f85874d2d3cbfb1daf41908e58caf3ec21fca081
Tom Nygren <tomnygren@guzz.com>
2023-05-25 16:28:48 +0100

    Update website info

---
+++ app/src/views/basics/CreatorVerificationBasics.tsx
@@ -0,0 +1,71 @@
+import { FC } from "react";
+import { SignMessage } from "../../components/SignMessage";
+import { SendTransaction } from "../../components/SendTransaction";
+import { SendVersionedTransaction } from "../../components/SendVersionedTransaction";
+import {
+  Box,
+  ListItem,
+  OrderedList,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  UnorderedList,
+} from "@chakra-ui/react";
+import { StyledTabs } from "components/tabs/Tabs";
+
+export const CreatorVerificationBasics: FC = ({}) => {
+  return (
+    <Box sx={{maxWidth :"600px"}}>
+      {/* <h3 className="text-2xl">Unchanged</h3>
+      <Box>
+        <UnorderedList
+          sx={{
+            maxWidth: "600px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "start",
+          }}
+        >
+          <ListItem>Name</ListItem>
+        </UnorderedList>
+      </Box> */}
+      <h3 className="text-2xl">Changed</h3>
+      <Box>
+        Creator verification will be independent of royalties. Proposed flow is
+        as follows:
+        <OrderedList>
+          <ListItem>
+            Update authority establishes a list of permitted signers (can be
+            done either at collection level or overridden at mint level)
+          </ListItem>
+          <ListItem>
+            Signer signs the mint. The signatures are permanent and cannot be removed
+            once signed, akin to a physical signature on a work of art.
+          </ListItem>
+        </OrderedList>
+
+        Separating creator verification from royalties eliminates some odd workarounds like
+        having creators with 0% royalty share taking up UI space and incurring unnecessary rent.
+      </Box>
+      {/*      
+      <h3 className="text-2xl">Under discussion</h3>
+      <Box>
+        <UnorderedList
+          sx={{
+            maxWidth: "600px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "start",
+          }}
+        >
+          <ListItem>Delegation types</ListItem>
+          <ListItem>
+            Royalty enforcement mechanism (token-2022 -v- metadata)
+          </ListItem>
+        </UnorderedList>
+      </Box> */}
+    </Box>
+  );
+};
---
+++ app/src/views/basics/index.tsx
@@ -0,0 +1,125 @@
+import { FC } from "react";
+import { SignMessage } from "../../components/SignMessage";
+import { SendTransaction } from "../../components/SendTransaction";
+import { SendVersionedTransaction } from "../../components/SendVersionedTransaction";
+import {
+  Box,
+  ListItem,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  UnorderedList,
+} from "@chakra-ui/react";
+import { StyledTabs } from "components/tabs/Tabs";
+import { MetadataBasics } from "./MetadataBasics";
+import { CollectionBasics } from "./CollectionBasics";
+import { RoyaltiesBasics } from "./RoyaltiesBasics";
+import { CreatorVerificationBasics } from "./CreatorVerificationBasics";
+
+export const BasicsView: FC = ({}) => {
+  return (
+    <div className="md:hero mx-auto p-4">
+      <div className="md:hero-content flex flex-col">
+        <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mt-10 mb-8">
+          Basics
+        </h1>
+        {/* CONTENT GOES HERE */}
+
+        <Box display="flex" columnGap={"20px"}>
+          <Box display="flex" flexDirection={"column"}>
+            <h5 className="text-center text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mb-8">
+              Design objectives
+            </h5>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                pb: 3,
+              }}
+            >
+              <UnorderedList
+                sx={{
+                  maxWidth: "400px",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignContent: "start",
+                  alignItems: "start",
+                }}
+              >
+                <ListItem>Retain existing functionality</ListItem>
+                <ListItem>Ease of on-boarding</ListItem>
+                <ListItem>Developer experience</ListItem>
+                <ListItem>Migration tools</ListItem>
+                <ListItem>Minimize rent</ListItem>
+                <ListItem>Incorporate new use cases</ListItem>
+                <ListItem>Future-proofing</ListItem>
+              </UnorderedList>
+            </Box>
+          </Box>
+
+          <StyledTabs>
+            <TabList>
+              <Tab>Metadata</Tab>
+              <Tab>Collections</Tab>
+              <Tab>Royalties</Tab>
+              <Tab>Creator verification</Tab>
+            </TabList>
+
+            <TabPanels>
+              <TabPanel
+                pl={'30px'}
+                sx={{
+                
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "start",
+                }}
+              >
+                <MetadataBasics />
+              </TabPanel>
+              <TabPanel
+                pl={'30px'}
+                sx={{
+                
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "start",
+                }}
+              >
+                <CollectionBasics />
+              </TabPanel>
+              <TabPanel
+                pl={'30px'}
+                sx={{
+                
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "start",
+                }}
+              >
+                <RoyaltiesBasics />
+              </TabPanel>
+              <TabPanel
+                pl={'30px'}
+                sx={{
+                
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "start",
+                }}
+              >
+                <CreatorVerificationBasics />
+              </TabPanel>
+            </TabPanels>
+          </StyledTabs>
+        </Box>
+        {/* <SignMessage />
+          <SendTransaction />
+          <SendVersionedTransaction /> */}
+      </div>
+    </div>
+  );
+};
