1744d93b166de9c8e493dd35c76987a1c75880b9
nilpferschaefer <2376484+nilpferdschaefer@users.noreply.github.com>
2023-06-02 16:39:33 +0100

    Add generic gpa fetcher and multi account fetcher. Refresh needs sorting out

--- app/src/components/demo/Demo.tsx
+++ app/src/components/demo/Demo.tsx
@@ -36,7 +36,7 @@ export const Demo = () => {
         }}
         rowGap={3}
       >
-        <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mt-10 mb-8">
+        <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mt-10 mb-10 pb-5">
           Metadata Manager
         </h1>
 
