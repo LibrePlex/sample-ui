9824a14864f75f562ea7659114e95c16b816789f
nilpferschaefer <2376484+nilpferdschaefer@users.noreply.github.com>
2023-07-02 00:59:10 +0100

    Fix capitalisation

--- app/src/components/tools/Tools.tsx
+++ app/src/components/tools/Tools.tsx
@@ -29,7 +29,7 @@ export const Tools = () => {
       rowGap={3}
     >
       <h1 className="text-center text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-indigo-500 to-fuchsia-500 mt-10 mb-10 pb-5">
-        Free tools from the LibrePlex Team.
+        Free Tools from the LibrePlex Team.
       </h1>
       <Box width="100%" display="flex" flexDir={"column"} alignItems={"center"}>
         <Stack maxWidth={"500px"}>
