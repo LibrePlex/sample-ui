83cd350fbaf1b4e6dba0310af839007365364caf
nilpferschaefer <2376484+nilpferdschaefer@users.noreply.github.com>
2023-07-15 00:35:41 +0100

    Remove ShopOwnerContext as this is no longer needed

--- marketplace/src/pages/_app.tsx
+++ marketplace/src/pages/_app.tsx
@@ -1,31 +1,28 @@
-import type { AppProps } from "next/app";
 import {
   ChakraProvider,
   PortalManager,
   createLocalStorageManager,
 } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
 import Head from "next/head";
+import { FC, useMemo, useState } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import {
   ContextProvider,
   InscriptionsProgramProvider,
   LibrePlexProgramProvider,
   Notifications,
 } from "shared-ui";
-import { FC, useMemo, useState } from "react";
+import {
+  LibrePlexShopProgramProvider
+} from "shared-ui/src/anchor";
 import { AppBar } from "../components/AppBar";
-import React from "react";
 import { ContentContainer } from "../components/ContentContainer";
+import {
+  ShopOwnerProvider
+} from "../components/ShopOwnerContext";
 require("@solana/wallet-adapter-react-ui/styles.css");
 require("../styles/globals.css");
-import { QueryClient, QueryClientProvider } from "react-query";
-import {
-  LibrePlexShopProgramContext,
-  LibrePlexShopProgramProvider,
-} from "shared-ui/src/anchor";
-import {
-  ShopOwnerContext,
-  ShopOwnerProvider,
-} from "../components/ShopOwnerContext";
 const manager = createLocalStorageManager("colormode-key");
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
@@ -42,7 +39,6 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
         <QueryClientProvider client={queryClient}>
           <ContextProvider>
             <div className="flex flex-col h-screen bg-[#121212]">
-              <ShopOwnerProvider>
                 <Notifications />
 
                 <AppBar isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
@@ -57,7 +53,6 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
                     </LibrePlexShopProgramProvider>
                   </InscriptionsProgramProvider>
                 </LibrePlexProgramProvider>
-              </ShopOwnerProvider>
             </div>
           </ContextProvider>
         </QueryClientProvider>
