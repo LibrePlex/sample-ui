8d79e91fcc08319fb94568df4777333085a8c3a1
AppsusUK <suraj@appsus.co.uk>
2023-09-30 00:25:12 +0100

    added head to mint page

--- marketplace/src/pages/mint/[mintId]/index.tsx
+++ marketplace/src/pages/mint/[mintId]/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { MintDisplay } from "@libreplex/shared-ui"
 import { PublicKey } from "@solana/web3.js"
 import { Center, Text } from '@chakra-ui/react'
+import Head from 'next/head'
 
 type Props = {}
 
@@ -10,11 +11,16 @@ function MintDisplayPage({}: Props) {
     const router = useRouter()
 
     return (
+        <>
+        <Head>
+            <title>LibrePlex - Mint {router.query.mintId}</title>
+        </Head>
         <Center m={12}>
             {router.query.mintId ? <MintDisplay mint={new PublicKey(router.query.mintId)}/> : <Text>
                 No mint id specified
             </Text>}
         </Center>
+        </>
     )
 }
 
